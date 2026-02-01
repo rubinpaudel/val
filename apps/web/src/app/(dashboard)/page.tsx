cee5c365b3ab9e08fa095c037860dddc357737a1
Rubin Paudel <paudelrubin18@gmail.com>
2026-02-01 19:19:03 +0100

    add ai-elements

--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -1,15 +1,10 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
 
-import { trpc } from "@/utils/trpc";
-
-export default function Home() {
-  const healthCheck = useQuery(trpc.healthCheck.queryOptions());
+export default function Dashboard() {
 
   return (
-    <div className="container mx-auto max-w-3xl px-4 py-2">
-      <div className="grid gap-6">
-      </div>
+    <div className="">
+      Got a shower thought?
     </div>
   );
 }
